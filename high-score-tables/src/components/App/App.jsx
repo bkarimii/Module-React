916abc567ad04f1d707fed7e97435994a6887569
karimi <behrouzkarimi1993@gmail.com>
2024-02-28 14:03:00 +0000

    Button created for toggle scoresmwithout functionality

--- high-score-tables/src/components/App/App.jsx
+++ high-score-tables/src/components/App/App.jsx
@@ -2,11 +2,13 @@ import HighScoreTable from "@/components/HighScoreTable/HighScoreTable.jsx";
 import scores from "@/data/scores.json";
 import "./App.scss";
 import Sorter from "../SorterCountryByName/SortCountryByName";
+import ButtonToggleScores from "../ButtonToggleScores/ButtonToggleScore";
 
 scores.sort(Sorter);
 const App = () => (
   <div className="app">
     <header className="app__header">
+      <ButtonToggleScores />
       <h1 className="app__heading">High Scores</h1>
     </header>
     {scores.map(({ name, scores }, index) => (
---
+++ high-score-tables/src/components/ButtonToggleScores/ButtonToggleScore.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ButtonToggleScores() {
+  return <button>Desc/Asce</button>;
+}
+
+export default ButtonToggleScores;
