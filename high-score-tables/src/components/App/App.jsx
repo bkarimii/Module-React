aca626d121f4c348e6dc428d45fcbf9b0c433dc3
karimi <behrouzkarimi1993@gmail.com>
2024-03-04 14:10:58 +0000

    add button to sort ascending and descending

--- high-score-tables/src/components/App/App.jsx
+++ high-score-tables/src/components/App/App.jsx
@@ -1,17 +1,35 @@
+// App.jsx
+import React, { useState } from "react";
 import HighScoreTable from "@/components/HighScoreTable/HighScoreTable.jsx";
 import scores from "@/data/scores.json";
+import SortScores from "../sortScores/SortScores";
 import "./App.scss";
 import Sorter from "../SorterCountryByName/SortCountryByName";
-import { useState } from "react";
 
-scores.sort(Sorter);
+const sortedByName = scores.sort((a, b) => Sorter(a, b));
 const App = () => {
+  const [isAscending, setIsAscending] = useState(false);
+
+  const toggleSorting = () => {
+    setIsAscending((prevState) => !prevState);
+  };
+
+  const sortedScores = sortedByName.map(({ name, scores }) => ({
+    name,
+    scores: [...scores].sort((a, b) =>
+      isAscending ? SortScores(a, b) : SortScores(b, a)
+    ),
+  }));
+
   return (
     <div className="app">
       <header className="app__header">
         <h1 className="app__heading">High Scores</h1>
+        <button onClick={toggleSorting}>
+          Sort {isAscending ? "Ascending" : "Descending"}
+        </button>
       </header>
-      {scores.map(({ name, scores }, index) => (
+      {sortedScores.map(({ name, scores }, index) => (
         <HighScoreTable country={name} scores={scores} key={index} />
       ))}
     </div>
--- high-score-tables/src/components/HighScoreTable/HighScoreTable.jsx
+++ high-score-tables/src/components/HighScoreTable/HighScoreTable.jsx
@@ -1,42 +1,24 @@
+// HighScoreTable.jsx
 import React from "react";
 import PlayerScore from "@/components/PlayerScore/PlayerScore.jsx";
 import "./HighScoreTable.scss";
-import SortScores from "../sortScores/SortScores";
-import ButtonToggleScores from "../ButtonToggleScores/ButtonToggleScore";
-import { useState } from "react";
 
-// https://react.dev/learn/rendering-lists
-
-// this is a start for level 2 chanlenge
-const HighScoreTable = ({ country, scores = [] }) => {
-  //let sortedScore = scores.sort((a, b) => SortScores(b, a));
-  const [isAscending, setIsAscending] = useState(false);
-
-  const ToggleButton = () => {
-    setIsAscending((isPrev) => !isPrev);
-  };
-
-  const sortScoresConditionally = scores.sort((a, b) =>
-    isAscending ? SortScores(a, b) : SortScores(b, a)
-  );
+const HighScoreTable = ({ country, scores }) => {
   return (
-    <>
-      <ButtonToggleScores onClick={ToggleButton} />
-      <table className="scores">
-        <caption>{country}</caption>
-        <thead>
-          <tr>
-            <th>Player</th>
-            <th>Score</th>
-          </tr>
-        </thead>
-        <tbody>
-          {sortScoresConditionally.map((score, index) => (
-            <PlayerScore key={index} name={score.n} score={score.s} />
-          ))}
-        </tbody>
-      </table>
-    </>
+    <table className="scores">
+      <caption>{country}</caption>
+      <thead>
+        <tr>
+          <th>Player</th>
+          <th>Score</th>
+        </tr>
+      </thead>
+      <tbody>
+        {scores.map((score, index) => (
+          <PlayerScore key={index} name={score.n} score={score.s} />
+        ))}
+      </tbody>
+    </table>
   );
 };
 
